468b74105ed4adae4c3947cca56eab7111f22f02
saybhat <119410444+saybhat@users.noreply.github.com>
2022-12-15 15:50:59 +0530

    Delete test.exs

--- config/test.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :assignment_first, ecto_repos: [First.Repo]
-config :assignment_first,First.Repo,
-  database: "assignment_first",
-  username: "sayan",
-  password: "sayan1231",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
